b4950227887c4022f1655b5bed26cbd3b16bf3b3
rendycodeigniter12 <rendycodeigniter12@gmail.com>
2023-01-09 10:59:30 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/belajarandroidrendy/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.belajarandroidrendy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setTitle("Belajar Android");
+    }
+}
